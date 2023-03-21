bae5c9057daa527f27ffb8accdc90b231795b5f2
YarKir <yarkirgen@mail.ru>
2023-03-21 22:45:29 +0500

    Ver 0.3 Fixed

--- src/main/java/org/example/MyController.java
+++ src/main/java/org/example/MyController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +17,9 @@ public class MyController {
     private Map<String, String> users = new HashMap<>();
 
     @RequestMapping("/")
-    public String ShowUsers() {
+    public String showUsers(Model model) {
+
+        model.addAttribute("usersMap", users);
 
         return "index";
     }
@@ -29,13 +33,16 @@ public class MyController {
     }
 
     @RequestMapping("/addUser")
-    public String addUsersToMap(@ModelAttribute("userProfile") UserProfile profile, Model model) {
+    public RedirectView addUsersToMap(@ModelAttribute("userProfile") UserProfile profile,
+                                      Model model, RedirectAttributes attributes) {
         if (profile.getName() != null) {
             users.put(profile.getName(), profile.getEmail());
         }
         model.addAttribute("usersMap", users);
 
-        return "index";
+        attributes.addFlashAttribute("flashAttribute", "addUser");
+        attributes.addAttribute("attribute", "addUser");
+        return new RedirectView("/");
     }
 
 }
